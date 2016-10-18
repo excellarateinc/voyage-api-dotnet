e1c0c711f6c4d4c5e4648ea3214d405366386936
jesse <jesse@DESKTOP-OM23VLV>
2016-10-18 11:05:00 -0500

    Setup services unit test project

---
+++ Launchpad.Services.UnitTests/WidgetServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Moq;
+using Launchpad.UnitTests.Common;
+using Launchpad.Data.Interfaces;
+using Launchpad.Models;
+using Ploeh.AutoFixture;
+using System.Linq;
+using FluentAssertions;
+
+namespace Launchpad.Services.UnitTests
+{
+
+    public class WidgetServiceTests : BaseUnitTest
+    {
+        private WidgetService _widgetService;
+        private Mock<IWidgetRepository> _mockWidgetRepository;
+
+        public WidgetServiceTests()
+        {
+            _mockWidgetRepository = Mock.Create<IWidgetRepository>();
+            _widgetService = new WidgetService(_mockWidgetRepository.Object);
+        }
+
+        [Fact]
+        public void GetWidgets_Should_Call_WidgetRepository()
+        {
+            //Arrange
+            var fakeWidgets = Fixture.CreateMany<Widget>().ToList();
+
+            _mockWidgetRepository.Setup(_ => _.GetAll())
+                .Returns(fakeWidgets.AsQueryable());
+         
+            //Act
+            var widgets = _widgetService.GetWidgets();
+
+            //Assert
+
+            //Verify the number of calls to the repo
+            _mockWidgetRepository.Verify(_ => _.GetAll(), Times.Once());
+
+            //Verify any expectations setup on mocks created from the factory
+            Mock.VerifyAll();
+
+            //Verify the data
+            widgets.Should().HaveSameCount(fakeWidgets);
+            widgets.All(_ => fakeWidgets.Any(fake => fake.Id.Equals(_.Id) && fake.Name.Equals(_.Name))).Should().BeTrue();
+            
+
+        }
+    }
+}
---
+++ Launchpad.UnitTests.Common/BaseUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Ploeh.AutoFixture;
+
+namespace Launchpad.UnitTests.Common
+{
+    public abstract class BaseUnitTest
+    {
+        /// <summary>
+        /// Provides access to a Mock Repository in unit tests
+        /// </summary>
+        protected MockRepository Mock;
+
+        /// <summary>
+        /// Provides access to Autofixture in unit tests
+        /// </summary>
+        protected Fixture Fixture;
+
+        public BaseUnitTest()
+        {
+            Fixture = new Fixture();
+            Mock = new MockRepository(MockBehavior.Strict);
+        }
+    }
+}
