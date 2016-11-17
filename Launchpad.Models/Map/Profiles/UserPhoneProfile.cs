9dacff94af656729264bf01637fc5b690cc318c8
jesse <jesse.arens@lssinc.com>
2016-11-17 14:31:32 -0600

    Updated for CI

--- Launchpad.Models/Map/Profiles/UserPhoneProfile.cs
+++ Launchpad.Models/Map/Profiles/UserPhoneProfile.cs
@@ -16,15 +16,15 @@ namespace Launchpad.Models.Map.Profiles
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.PhoneType, opt => opt.MapFrom(src => src.PhoneType))
-                //.ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User))
-                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                ;
 
             CreateMap<UserPhoneModel, UserPhone>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.PhoneType, opt => opt.MapFrom(src => src.PhoneType))
-                //.ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User))
-                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForAllOtherMembers(opt => opt.Ignore());
         }
     }
 }
