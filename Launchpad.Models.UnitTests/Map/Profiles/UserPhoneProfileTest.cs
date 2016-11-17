14256a77a55d7a654a4816da8ed839721585bcb7
alfredo ball <alfredo.ball@lssinc.com>
2016-11-17 09:07:50 -0600

    Added property initialization and assertions for UserPhoneEntity_Should_MapTo_UserPhoneModel

--- Launchpad.Models.UnitTests/Map/Profiles/UserPhoneProfileTest.cs
+++ Launchpad.Models.UnitTests/Map/Profiles/UserPhoneProfileTest.cs
@@ -1,4 +1,5 @@
-﻿using Launchpad.Models.EntityFramework;
+﻿using FluentAssertions;
+using Launchpad.Models.EntityFramework;
 using Launchpad.Models.UnitTests.Fixture;
 using Launchpad.UnitTests.Common;
 using System;
@@ -24,11 +25,21 @@ namespace Launchpad.Models.UnitTests.Map.Profiles
         [Fact]
         public void UserPhoneEntity_Should_MapTo_UserPhoneModel()
         {
-            var userPhone = new UserPhone();
+            var userPhone = new UserPhone()
+            {
+                Id = 1,
+                UserId = string.Empty,
+                PhoneNumber = "6125555555",
+                PhoneType = Enum.PhoneType.Office
+            };
 
             var model = _mappingFixture.MapperInstance.Map<UserPhoneModel>(userPhone);
 
-
+            model.Should().NotBeNull();
+            model.Id.Should().Be(userPhone.Id);
+            model.UserId.Should().Be(userPhone.UserId);
+            model.PhoneNumber.Should().Be(userPhone.PhoneNumber);
+            model.PhoneType.Should().Be(userPhone.PhoneType);
         }
     }
 }
