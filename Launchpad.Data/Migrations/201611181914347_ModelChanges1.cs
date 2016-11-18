a066bdd91c5eafae687c725fb9fe87a66af16905
jesse <jesse.arens@lssinc.com>
2016-11-18 13:15:51 -0600

    Fix the ef model

---
+++ Launchpad.Data/Migrations/201611181914347_ModelChanges1.cs
@@ -0,0 +1,19 @@
+namespace Launchpad.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModelChanges1 : DbMigration
+    {
+        public override void Up()
+        {
+           
+            
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
