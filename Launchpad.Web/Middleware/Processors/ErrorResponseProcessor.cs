ebafe0ea7a98cf523616f3419590bf74b033623d
jesse <jesse.arens@lssinc.com>
2016-11-22 08:41:13 -0600

    Added comments

--- Launchpad.Web/Middleware/Processors/ErrorResponseProcessor.cs
+++ Launchpad.Web/Middleware/Processors/ErrorResponseProcessor.cs
@@ -7,11 +7,11 @@ namespace Launchpad.Web.Middleware.Processors
         public override bool ShouldProcess(IOwinResponse response)
         {
             return
-                response.Body.CanSeek &&
-                response.Body.CanRead &&
-                response.StatusCode >= 400 &&
-                response.StatusCode <= 599 &&
-                response.ContentLength > 0 && (
+                response.Body.CanSeek && //Can the steam seek?
+                response.Body.CanRead && //can the stream read?
+                response.StatusCode >= 400 && //Request errors
+                response.StatusCode <= 599 && //Server errors
+                response.ContentLength > 0 && ( ///Readable content
                 response.ContentType.StartsWith("application/json") ||
                 response.ContentType.StartsWith("application/xml") ||
                 response.ContentType.StartsWith("text/"));
