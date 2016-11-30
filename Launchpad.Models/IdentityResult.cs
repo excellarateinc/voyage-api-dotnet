125e00cca9d551fbd0f5b1da379000daf0e36f68
jesse <jesse.arens@lssinc.com>
2016-11-30 14:19:17 -0600

    Refactored into class files

--- Launchpad.Models/IdentityResult.cs
+++ Launchpad.Models/IdentityResult.cs
@@ -1,63 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
-using System.Collections.Generic;
 
 namespace Launchpad.Models
 {
-
-
-
-    public class EntityResult
-    {
-        public List<string> Errors { get; }
-
-        public bool Succeeded { get; }
-
-        public bool IsMissingEntity { get; }
-
-        public EntityResult(bool succeeded, bool isMissingEntity, params string[] errors)
-        {
-            Succeeded = succeeded;
-            IsMissingEntity = isMissingEntity;
-            Errors = new List<string>();
-            Errors.AddRange(errors);
-        }
-
-        public EntityResult WithErrorCodeMessage(string code, string message)
-        {
-            Errors.Add(string.Format("{0}::{1}", code, message));
-            return this;
-        }
-
-        public EntityResult WithMissingEntity(object id)
-        {
-            return this.WithErrorCodeMessage(Constants.ErrorCodes.EntityNotFound, $"Could not locate entity with ID {id}");
-        }
-
-    }
-
-    public class EntityResult<TModel> : EntityResult where TModel : class
-    {
-        public TModel Model { get; }
-
-        public EntityResult(TModel model, bool succeeded, bool isMissingEntity, params string[] errors)
-            : base(succeeded, isMissingEntity, errors)
-        {
-            Model = model;
-        }
-
-        public new EntityResult<TModel> WithErrorCodeMessage(string code, string message)
-        {
-            Errors.Add(string.Format("{0}::{1}", code, message));
-            return this;
-        }
-
-        public new EntityResult<TModel> WithMissingEntity(object id)
-        {
-            return this.WithErrorCodeMessage(Constants.ErrorCodes.EntityNotFound, $"Could not locate entity with ID {id}");
-        }
-    }
-
-
     public class IdentityResult<TModel>
     {
         public IdentityResult(IdentityResult result, TModel model)
---
+++ Launchpad.Services/EntityResultService.cs
@@ -0,0 +1,47 @@
+﻿using Launchpad.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+
+namespace Launchpad.Services
+{
+    public abstract class EntityResultService
+    {
+        protected EntityResult Missing(object id)
+        {
+            return new EntityResult(false, true)
+              .WithMissingEntity(id);
+        }
+
+        protected EntityResult<TModel> Missing<TModel>(object id)
+            where TModel : class
+        {
+            return new EntityResult<TModel>(null, false, true)
+              .WithMissingEntity(id);
+        }
+
+
+        protected EntityResult<TModel> Success<TModel>(TModel model)
+            where TModel : class
+        {
+            return new EntityResult<TModel>(model, true, false);
+        }
+
+        protected EntityResult Success()
+        {
+            return new EntityResult(true, false);
+        }
+
+
+        protected EntityResult<TModel> FromIdentityResult<TModel>(IdentityResult result, TModel model)
+             where TModel : class
+        {
+            return new EntityResult<TModel>(model, result.Succeeded, false, result.Errors.ToArray());
+        }
+
+        protected EntityResult FromIdentityResult(IdentityResult result)
+        {
+            return new EntityResult(result.Succeeded, false, result.Errors.ToArray());
+        }
+
+    }
+}
