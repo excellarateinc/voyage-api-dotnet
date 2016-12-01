0c26ad85c258e624bd1f5f6b9a7c5b83ba666f5c
jesse <jesse.arens@lssinc.com>
2016-12-01 09:48:38 -0600

    Cleaned up dependencies in controller

--- Launchpad.Web/Controllers/API/v1/ApplicationInfoController.cs
+++ Launchpad.Web/Controllers/API/v1/ApplicationInfoController.cs
@@ -1,37 +1,20 @@
 ﻿using Launchpad.Core;
-using Launchpad.Models;
-using Launchpad.Models.Enum;
 using Launchpad.Services.Interfaces;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Configuration;
 using System.Web.Http;
 
 namespace Launchpad.Web.Controllers.API.V1
 {
-   
+
     [RoutePrefix(Constants.RoutePrefixes.V1)]
     [AllowAnonymous]
     public class ApplicationInfoController : ApiController
     {
         private readonly IApplicationInfoService _applicationInfoService;
-        private readonly IConfigurationManagerService _configurationManagerService;
-        private readonly IFileReaderService _fileReaderService;
-        private readonly IPathProviderService _pathProviderService;
 
-        public ApplicationInfoController(IApplicationInfoService applicationInfoService, IConfigurationManagerService configurationManagerService, IFileReaderService fileReaderService, IPathProviderService pathProviderService)
+        public ApplicationInfoController(IApplicationInfoService applicationInfoService)
         {
             _applicationInfoService = applicationInfoService.ThrowIfNull(nameof(applicationInfoService));
-            _configurationManagerService = configurationManagerService.ThrowIfNull(nameof(configurationManagerService));
-            _fileReaderService = fileReaderService.ThrowIfNull(nameof(fileReaderService));
-            _pathProviderService = pathProviderService.ThrowIfNull(nameof(pathProviderService));
+
         }
 
         /**
@@ -54,7 +37,7 @@ namespace Launchpad.Web.Controllers.API.V1
         public IHttpActionResult Get()
         {
             var appInfo = _applicationInfoService.GetApplicationInfo();
-            
+
             return Ok(appInfo);
         }
     }
