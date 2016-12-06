d520b0ea78e1c5b6050a251cfe0a4821273e60fc
jesse <jesse.arens@lssinc.com>
2016-12-06 16:48:22 -0600

    Added a bat file that Jenkins can call to deploy the package

---
+++ deploy-package.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:: Need to install - https://msdn.microsoft.com/en-us/library/mt204009.aspx
+:: DacFx
+
+
+SETLOCAL ENABLEEXTENSIONS
+
+:: root is the folder containing this script (without trailing backslash)
+set root=%~dp0
+set root=%root:~0,-1%
+
+::Path to the SqlPackage.exe
+SET toolPath="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\SqlPackage.exe"
+
+::Path to the dacpac
+SET sourcePath=".\Launchpad.Database.dacpac"
+
+::Connection string for the target
+SET connectionString="Integrated Security=SSPI;Persist Security Info=False;Data Source=localhost;Initial Catalog=Launchpad"
+
+:: process command line
+if not [%1]==[] if not [%1]==[-] set toolPath=%1
+if not [%2]==[] if not [%2]==[-] set sourcePath=%2
+if not [%3]==[] if not [%3]==[-] set connectionString=%3
+
+
+:: report configuration
+echo connectionString:   %connectionString%
+echo sourcePath: %sourcePath%
+echo toolPath: %toolPath%
+
+%toolPath% /TargetConnectionString:%connectionString% /SourceFile:%sourcePath% /Action:Publish
+
