99b1eb047e551f2c7ce404ddfddf99f0102a23a8
Tim Michalski <tim@lssinc.com>
2016-12-11 19:56:04 -0600

    Create apidoc-header.js

---
+++ Launchpad.Web/apidoc/apidoc-header.js
@@ -0,0 +1,20 @@
+// ------------------------------------------------------------------------------------------
+// Common apiDoc elements
+// -- Reusable definitions
+// -- Current version of an API definition
+// -- Historical versions of API definitions (for comparison, if necessary)
+// ------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------
+// Auth Header
+// ------------------------------------------------------------------------------------------
+/**
+ * @apiDefine AuthHeader
+ *
+ * @apiHeader {String} Authorization Authentication token returned from the /api/Token service
+ *
+ * @apiHeaderExample Header-Example:
+ * {
+ *     "Authorization": "bearer 5_leQJfkDVIFxRgt-YZcnRGvzlrsFdNXClRaZEaTf3jzToaoFAUtOyneKNhtYo2cCSC9_fYXXBMS3UHwY3MZuzRbWmLqG_H1T5Cm6YHUzzQHPpKCCUqcYMx6m3NMLQoGVX9_f2svSSY4HieOTzwT-LTfxYbxHyuiRY0sJrmoGnQOtWGGC9GhdUXPCpM9_1L8p0vdAC1wnnTKtkDkxulAUkIxj3jJIUc9lUhqLoh5BRYJ8VsABsKEZYouLWvYfL5eqkL7EjwtxRNBhy9rJSU7nc_6lNCk5PEpsFg-laLYWeVO10gVFBkI3Ml7I8KgJA2Bs3vKKccIxuOwqoWjU_XIhV3f9Lhjm34OqSrwcThfx940k5TEWjHG9CHqxnItRvgHtqahc2rP8q0QuZ0qw6Ph5kxEE18YVGa8IVX7zAPW8V6WtXnVnR7SbZwnwiNTuh3i0t0Ib_gOKZAwUZTrVhsTqnFe20zMvJIsOzTtijl80bJMF7ZBAbKKL6zKfJcQ_DwYJaM0CISapXGjbJYMNWPaOzg6kTKtVuHb4akxfxlsAUk0QFqxZ793TT-KCdh-c0ppUNb3brPMh8wGpJDhwLbnhUfpBVnuxdC-sDrGUtzpSnMrPavKivovVT-sXBbqzeg6udOhjN3JXIv2ZzctisLYTw"
+ * }
+ */
