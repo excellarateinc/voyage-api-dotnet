afff25b1d45e3c32fa384a97059498772a2be1bc
Jon Raymer <jon.raymer@lssinc.com>
2017-02-07 17:43:27 -0600

    Remove external oauth support for now.

--- Voyage.Web/Controllers/AccountController.cs
+++ Voyage.Web/Controllers/AccountController.cs
@@ -32,34 +32,5 @@ namespace Voyage.Web.Controllers
         {
             return View();
         }
-
-        public ActionResult External()
-        {
-            var authentication = HttpContext.GetOwinContext().Authentication;
-            if (Request.HttpMethod == "POST")
-            {
-                foreach (var key in Request.Form.AllKeys)
-                {
-                    if (key.StartsWith("submit.External.") && !string.IsNullOrEmpty(Request.Form.Get(key)))
-                    {
-                        var authType = key.Substring("submit.External.".Length);
-                        authentication.Challenge(authType);
-                        return new HttpUnauthorizedResult();
-                    }
-                }
-            }
-
-            var identity = authentication.AuthenticateAsync("External").Result.Identity;
-            if (identity != null)
-            {
-                authentication.SignOut("External");
-                authentication.SignIn(
-                    new AuthenticationProperties { IsPersistent = true },
-                    new ClaimsIdentity(identity.Claims, "Application", identity.NameClaimType, identity.RoleClaimType));
-                return Redirect(Request.QueryString["ReturnUrl"]);
-            }
-
-            return View();
-        }
     }
 }
