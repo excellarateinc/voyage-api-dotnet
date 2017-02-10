da3712ba15befd81d968b6f5b84c4369bcdb7a5c
Jon Raymer <jon.raymer@lssinc.com>
2017-02-09 20:07:45 -0600

    UI updates

--- Voyage.Web/AuthProviders/ApplicationTokenProvider.cs
+++ Voyage.Web/AuthProviders/ApplicationTokenProvider.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Microsoft.Owin.Security.Infrastructure;
 
 namespace Voyage.Web.AuthProviders
