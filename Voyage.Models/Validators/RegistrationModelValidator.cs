cf73fc14958df124b22d9ed62a6fce7bca734c6f
ajay nagaboina <ajay.nagaboina@lssinc.com>
2017-04-24 15:12:06 +0530

    unit testing

--- Voyage.Models/Validators/RegistrationModelValidator.cs
+++ Voyage.Models/Validators/RegistrationModelValidator.cs
@@ -35,7 +35,9 @@ namespace Voyage.Models.Validators
 
             RuleFor(_ => _.PhoneNumber)
                 .NotEmpty()
-                .WithErrorCodeMessage(Constants.ErrorCodes.MissingField, "Phone Number is required field");
+                .WithErrorCodeMessage(Constants.ErrorCodes.MissingField, "Phone Number is required field")
+                .Matches(@"(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2‌​-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9]‌​[02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})")
+                .WithErrorCodeMessage(Constants.ErrorCodes.InvalidPhoneNumber, "Invalid Phone number");
         }
     }
 }
