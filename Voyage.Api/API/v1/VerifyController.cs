837dbdaeb3de0cb97b3a1771c5c58d79e9343110
ajay nagaboina <ajay.nagaboina@lssinc.com>
2017-05-05 18:46:32 +0530

    send security code

---
+++ Voyage.Api/API/v1/VerifyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Voyage.Api.Filters;
+using Voyage.Core;
+using Voyage.Services.Phone;
+
+namespace Voyage.Api.API.v1
+{
+    [RoutePrefix(Constants.RoutePrefixes.V1)]
+    public class VerifyController : ApiController
+    {
+        private readonly IPhoneService _phoneService;
+
+        public VerifyController(IPhoneService phoneService)
+        {
+            _phoneService = phoneService.ThrowIfNull(nameof(phoneService));
+        }
+
+        /**
+        * @api {get} /v1/Verify/send Send verify code
+        * @apiVersion 0.1.0
+        * @apiName PostVerifySend
+        * @apiGroup Verify
+        *
+        * @apiPermission /**
+        * @api {get} /v1/users Get all users
+        * @apiVersion 0.1.0
+        * @apiName GetUsers
+        * @apiGroup User
+        *
+        * @apiPermission authenticated
+        *
+        * @apiUse AuthHeader
+        *
+        * @apiSuccessExample Success-Response:
+        *   HTTP/1.1 204 NO CONTENT
+        *
+        * @apiUse UnauthorizedError
+        **/
+       
+    }
+}
