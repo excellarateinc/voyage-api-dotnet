fe5aed781346caddd0a8ef68a0ca0c021766e410
Nu <maniphan@gmail.com>
2017-05-05 09:45:41 -0500

    Create new error constants for code verification and users that are disabled

--- Voyage.Core/EventCodes.cs
+++ Voyage.Core/EventCodes.cs
@@ -5,5 +5,7 @@
         public const string ActivityAudit = "Activity";
         public const string Authorization = "Authorization";
         public const string EntityValidation = "EntityValidation";
+        public const string RequireVerification = "RequireVerification";
+        public const string UserDisabled = "UserDisabled";
     }
 }
