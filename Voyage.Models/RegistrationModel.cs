7ff50bf526c12b442ac7c645d2d8e6d3740e5177
ajay nagaboina <ajay.nagaboina@lssinc.com>
2017-05-10 19:08:13 +0530

    commit

--- Voyage.Models/RegistrationModel.cs
+++ Voyage.Models/RegistrationModel.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation.Attributes;
+﻿using Embarr.WebAPI.AntiXss;
+using FluentValidation.Attributes;
 using System.Collections.Generic;
 using Voyage.Models.Validators;
 
@@ -7,18 +8,25 @@ namespace Voyage.Models
     [Validator(typeof(RegistrationModelValidator))]
     public class RegistrationModel
     {
+        [AntiXss]
         public string UserName { get; set; }
 
+        [AntiXss]
         public string Email { get; set; }
 
+        [AntiXss]
         public string Password { get; set; }
 
+        [AntiXss]
         public string ConfirmPassword { get; set; }
 
+        [AntiXss]
         public string FirstName { get; set; }
 
+        [AntiXss]
         public string LastName { get; set; }
 
+        [AntiXss]
         public List<UserPhoneModel> PhoneNumbers { get; set; }
     }
 }
