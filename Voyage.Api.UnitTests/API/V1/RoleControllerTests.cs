4d1673f26499457e9cbbbe3dba1df117e790e10f
ajay nagaboina <ajay.nagaboina@lssinc.com>
2017-07-24 15:36:26 +0530

    commiting

--- Voyage.Api.UnitTests/API/V1/RoleControllerTests.cs
+++ Voyage.Api.UnitTests/API/V1/RoleControllerTests.cs
@@ -9,7 +9,8 @@ using FluentAssertions;
 using Microsoft.AspNet.Identity;
 using Moq;
 using Ploeh.AutoFixture;
-using Voyage.Api.API.V1;
+using Voyage.Api.UserManager;
+using Voyage.Api.UserManager.API.V1;
 using Voyage.Api.UnitTests.Common;
 using Voyage.Core.Exceptions;
 using Voyage.Models;
@@ -231,7 +232,7 @@ namespace Voyage.Api.UnitTests.API.V1
         {
             typeof(RoleController)
                 .Should()
-                .BeDecoratedWith<RoutePrefixAttribute>(value => value.Prefix == Constants.RoutePrefixes.V1);
+                .BeDecoratedWith<RoutePrefixAttribute>(value => value.Prefix == RoutePrefixConstants.RoutePrefixes.V1);
         }
 
         [Fact]
@@ -451,7 +452,7 @@ namespace Voyage.Api.UnitTests.API.V1
 
             // Assert
             var message = await result.ExecuteAsync(new CancellationToken());
-            message.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            message.StatusCode.Should().Be(HttpStatusCode.OK);
             Mock.VerifyAll();
         }
     }
