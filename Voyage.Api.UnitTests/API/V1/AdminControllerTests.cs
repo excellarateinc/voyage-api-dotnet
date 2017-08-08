033639b0e8b0130101c37f4d73cbeb29adf3a6ad
Rajesh <rajesh.panda@lssinc.com>
2017-08-08 16:38:25 +0530

    Added Admin controller with the required status toggle change and test cases

---
+++ Voyage.Api.UnitTests/API/V1/AdminControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Moq;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Voyage.Api.API.V1;
+using Voyage.Api.UnitTests.Common;
+using Voyage.Core.Exceptions;
+using Voyage.Models;
+using Voyage.Services.User;
+using Xunit;
+
+namespace Voyage.Api.UnitTests.API.V1
+{
+    public class AdminControllerTests : BaseUnitTest
+    {
+        private readonly AdminController _controller;
+        private readonly Mock<IUserService> _mockUserService;
+        public AdminControllerTests()
+        {
+            _mockUserService = Mock.Create<IUserService>();
+
+            _controller = new AdminController(_mockUserService.Object)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+        }
+
+        [Fact]
+        public void AdminController_Constructor_ParametersAreNull_ShouldThrowArgumentNullException()
+        {
+            Action throwAction = () => new AdminController(null);
+
+            throwAction.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async void AdminController_ToggleAccountStatus_ShouldCallServiceToChangeUserStatus()
+        {
+            var userModel = Fixture.Create<UserModel>();
+            var returnModel = Fixture.Create<UserModel>();
+            var changeAccountStatusModel = Fixture.Create<ChangeAccountStatusModel>();
+            var id = Fixture.Create<string>();
+
+            _mockUserService.Setup(_ => _.GetUserAsync(id)).ReturnsAsync(userModel);
+
+            _mockUserService.Setup(_ => _.UpdateUserAsync(id, userModel))
+                .ReturnsAsync(returnModel);
+            
+            var result = await _controller.ToggleAccountStatus(id, changeAccountStatusModel);
+            
+            Mock.VerifyAll();
+        }
+
+        [Fact]
+        public void AdminController_ToggleAccountStatus_ShouldFailForEmptyStringId()
+        {
+            var changeAccountStatusModel = Fixture.Create<ChangeAccountStatusModel>();
+            var id = "";
+            
+            Assert.ThrowsAsync<BadRequestException>(async () => await _controller.ToggleAccountStatus(id, changeAccountStatusModel));
+        }
+
+    }
+}
---
+++ Voyage.Api/API/v1/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using Voyage.Core;
+using System.Web.Http;
+using Voyage.Services.User;
+using Voyage.Models;
+using System.Threading.Tasks;
+using System;
+using Voyage.Api.Filters;
+
+namespace Voyage.Api.API.V1
+{
+    [RoutePrefix(Constants.RoutePrefixes.V1)]
+    public class AdminController : ApiController
+    {
+        private readonly IUserService _userService;
+
+        public AdminController(IUserService userService)
+        {
+            _userService = userService.ThrowIfNull(nameof(userService));
+        }
+
+        /**
+        * @api {post} /v1/users/{userId}/account-status Update users' account status
+        * @apiVersion 1.0.0
+        * @apiName ToggleAccountStatus
+        * @apiGroup User
+        * 
+        * @apiPermission api.users.update
+        * 
+        * @apiParam {String} userId User ID
+        * @apiParamExample {json} Request-Example:
+        *       {
+        *          "isActive": true,
+        *          "IsVerifyRequired": false
+        *       }
+        * 
+        * @apiUse UserSuccessModel
+        * @apiUse BadRequestError
+        **/
+        [ClaimAuthorize(ClaimValue = AppClaims.UpdateUser)]
+        [HttpPost]
+        [Route("users/{userId}/account-status")]
+        public async Task<IHttpActionResult> ToggleAccountStatus([FromUri] string userId, [FromBody] ChangeAccountStatusModel changeAccountStatusModel)
+        {
+            if (string.IsNullOrEmpty(userId))
+                return BadRequest();
+            try
+            {
+                var user = await _userService.GetUserAsync(userId);
+                user.IsActive = changeAccountStatusModel.IsActive == null ? user.IsActive : (bool)changeAccountStatusModel.IsActive;
+                user.IsVerifyRequired = changeAccountStatusModel.IsVerifyRequired == null ? user.IsVerifyRequired : (bool)changeAccountStatusModel.IsVerifyRequired;
+                var result = await _userService.UpdateUserAsync(userId, user);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
