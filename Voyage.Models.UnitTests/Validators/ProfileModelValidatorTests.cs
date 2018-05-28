4faca55e11051278929dd7fa26a40ad542d045b8
Jon Raymer <jon.raymer@lssinc.com>
2018-05-28 11:13:51 -0500

    profile validator tests

---
+++ Voyage.Models.UnitTests/Validators/ProfileModelValidatorTests.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using FluentValidation.TestHelper;
+using Voyage.Models.UnitTests.Common;
+using Voyage.Models.Validators;
+using Xunit;
+
+namespace Voyage.Models.UnitTests.Validators
+{
+    [Trait("Category", "Model.Validation")]
+    public class ProfileModelValidatorTests : BaseUnitTest
+    {
+        private readonly ProfileModelValidator _validator;
+
+        public ProfileModelValidatorTests()
+        {
+            _validator = new ProfileModelValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_CurrentPassword_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(
+                model => model.CurrentPassword,
+                new ProfileModel
+                {
+                    NewPassword = "password!!!"
+                });
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Not_Changing_Password_And_CurrentPassword_Is_Null()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(
+                model => model.CurrentPassword,
+                new ProfileModel
+                {
+                    NewPassword = null
+                });
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Is_Too_Short()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "abc");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Is_Too_Long()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Has_No_Special_Characters()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "Hellooo");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Has_No_Uppercase_Letter()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "hellooo");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Has_No_Lowercase_Letter()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "HELLLO");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Changing_Password_And_Password_Has_No_Digit()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.NewPassword, "Hellooo");
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Changing_Password_With_Valid_Password()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(model => model.NewPassword, "Hello!1");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_ConfirmPassword_Does_Not_Match_Password()
+        {
+            _validator.ShouldHaveValidationErrorFor(
+                model => model.ConfirmNewPassword,
+                new ProfileModel
+                {
+                    FirstName = "first",
+                    LastName = "last",
+                    Email = "first.last@firstlast.com",
+                    NewPassword = "password!!!",
+                    ConfirmNewPassword = "notpassword!!",
+                    Phones = new List<UserPhoneModel>
+                                    {
+                                        new UserPhoneModel
+                                        {
+                                            PhoneNumber=string.Empty
+                                        }
+                                    }
+                });
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_ConfirmPassword_Matches_Password()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(
+                model => model.ConfirmNewPassword,
+                new ProfileModel
+                {
+                    FirstName = "first",
+                    LastName = "last",
+                    Email = "first.last@firstlast.com",
+                    NewPassword = "password!!!",
+                    ConfirmNewPassword = "password!!!",
+                    Phones = new List<UserPhoneModel>
+                                    {
+                                        new UserPhoneModel
+                                        {
+                                            PhoneNumber=string.Empty
+                                        }
+                                    }
+                });
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_FirstName_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.FirstName, null as string);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_LastName_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.LastName, null as string);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Invalid()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.Email, "abc");
+        }
+
+        [Fact]
+        public void Should_Have_Error_when_PhoneNumber_Is_Null()
+        {
+            _validator.ShouldHaveValidationErrorFor(model => model.Phones, new List<UserPhoneModel>());
+        }
+    }
+}
